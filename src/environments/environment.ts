23b1724f16ab697028f86bdecf1a8d9a083f5c8b
Clarence Anne <burgosclarens@gmail.com>
2018-02-05 18:59:40 +0800

    Modified environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+  	apiKey: "AIzaSyAoja4sLkLXg2fKctcqD_UAPXCQGIz484g",
+    authDomain: "midexam-7f8d4.firebaseapp.com",
+    databaseURL: "https://midexam-7f8d4.firebaseio.com",
+    projectId: "midexam-7f8d4",
+    storageBucket: "midexam-7f8d4.appspot.com",
+    messagingSenderId: "834925771757"
   }
 };
